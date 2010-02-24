d9a350060f05e033ab698a6ab5ad873904fa6f93
fforw@gmx.de <fforw@gmx.de@b28efbb6-974e-0410-a7d0-456cb01d9dc4>
2010-02-24 18:46:13 +0000

    git-svn-id: http://jcouchdb.googlecode.com/svn/trunk@249 b28efbb6-974e-0410-a7d0-456cb01d9dc4

---
+++ test/org/jcouchdb/BulkWriteBugTestCase.java
@@ -0,0 +1,40 @@
+package org.jcouchdb;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jcouchdb.db.Database;
+import org.jcouchdb.db.LocalDatabaseTestCase;
+import org.jcouchdb.document.BaseDocument;
+import org.jcouchdb.document.DocumentInfo;
+import org.junit.Test;
+
+public class BulkWriteBugTestCase
+{
+    @Test
+    public void test()
+    {
+        Database db = LocalDatabaseTestCase.recreateDB("bulk-bug");
+        
+        final int num_docs = 1000;
+        List<BaseDocument> documents = new ArrayList<BaseDocument>(num_docs);
+        for (int i=0; i < num_docs; i++)
+        {
+            BaseDocument doc = new BaseDocument();
+            doc.setProperty("data", "The quick brown fox jumps over the lazy dog.");
+            doc.setId(null);
+            doc.setRevision(null);
+            documents.add(doc);
+        }
+        
+        List<DocumentInfo> infos = db.bulkCreateDocuments(documents);
+        assertThat(infos, is(notNullValue()));
+        assertThat(infos.size(), is(num_docs));
+        
+    }
+
+}
